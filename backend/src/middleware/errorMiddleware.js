8016cdd5d5efd1bd844f3ad84fca0c845df00451
subasthican <manoharansubasthican@gmail.com>
2026-02-20 14:00:34 +0530

    config env & error handling

--- backend/src/middleware/errorMiddleware.js
+++ backend/src/middleware/errorMiddleware.js
@@ -1,8 +1,13 @@
 const errorHandler = (err, req, res, next) => {
-  res.status(err.statusCode || 500).json({
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).json({
     success: false,
     data: null,
-    error: err.message || "Server Error"
+    error:
+      process.env.NODE_ENV === "production"
+        ? "Server Error"
+        : err.message
   });
 };
 

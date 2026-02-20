d0ac9e51e68f6045b1de534a0f8ec0323f7ae896
subasthican <manoharansubasthican@gmail.com>
2026-02-20 16:31:32 +0530

    configure CORS middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const authRoutes = require("./routes/authRoutes");

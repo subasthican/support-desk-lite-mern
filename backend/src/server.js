30772dcf3d8063346ba6cbbf52091f16c5936d1d
subasthican <manoharansubasthican@gmail.com>
2026-02-20 13:07:07 +0530

    add error handling and update the server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 const app = require("./app");
 const connectDB = require("./config/db");
+const { errorHandler } = require("./middleware/errorMiddleware");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 connectDB();
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

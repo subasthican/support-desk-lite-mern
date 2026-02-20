f42b0832541f197760ea71b2f43e196e519c7372
subasthican <manoharansubasthican@gmail.com>
2026-02-20 09:54:28 +0530

    app.js fixed v1

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,11 +5,13 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
 const authRoutes = require("./routes/authRoutes");
 
 app.use("/api/auth", authRoutes);
 
-app.use(express.json());
+
 
 app.get("/", (req, res) => {
   res.json({

c78574ac4900aa52c5520cd7e89c5bbc2075e526
subasthican <manoharansubasthican@gmail.com>
2026-02-20 12:08:32 +0530

    add validation for comment

---
+++ backend/src/validations/commentValidation.js
@@ -0,0 +1,8 @@
+const Joi = require("joi");
+
+const createCommentSchema = Joi.object({
+  body: Joi.string().min(1).max(2000).required(),
+  type: Joi.string().valid("public", "internal").required()
+});
+
+module.exports = { createCommentSchema };

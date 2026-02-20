683f172e2dc4634d72e4f46affe927a657da590e
subasthican <manoharansubasthican@gmail.com>
2026-02-20 13:31:49 +0530

    add helmet and mongo-sanitize

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,16 @@
 const app = require("./app");
 const connectDB = require("./config/db");
 const mongoose = require("mongoose");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
 const { errorHandler } = require("./middleware/errorMiddleware");
 
 const PORT = process.env.PORT || 5001;
 
 mongoose.set("strictQuery", true);
+
+app.use(helmet());
+app.use(mongoSanitize());
 
 connectDB();
 
